a057811e3c9d8883339ac910678548412bb05a64
Shahd  Mostafa <shahd.mustafa124@gmail.com>
2025-01-24 16:34:43 +0200

    Q1

--- Assignment 11 - C# - advanced - shahd mostafa/Program.cs
+++ Assignment 11 - C# - advanced - shahd mostafa/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace Assignment_11___C____advanced___shahd_mostafa
+﻿using Assignment_11___C____advanced___shahd_mostafa.classes;
+
+namespace Assignment_11___C____advanced___shahd_mostafa
 {
     internal class Program
     {
+        static void EmployeeLaidOffHandler(object sender, EmployeeLayOffEventArgs e)
+        {
+            Employee? emp = sender as Employee;
+            Console.WriteLine($"Employee {emp?.EmployeeID} is laid of due to {e.Cause}");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            Employee employee1 = new Employee() { EmployeeID = 11, BirthDate = new DateTime(2002, 5, 2), VacationStock = 5 };
+            Employee employee2 = new Employee() { EmployeeID = 13, BirthDate = new DateTime(2003, 3, 5), VacationStock = -1 };
+            Employee employee3 = new Employee() { EmployeeID = 14, BirthDate = new DateTime(1940, 4, 8), VacationStock = 7 };
+
+            employee1.EmployeeLayOff += EmployeeLaidOffHandler;
+            employee2.EmployeeLayOff += EmployeeLaidOffHandler;
+            employee3.EmployeeLayOff += EmployeeLaidOffHandler;
+
+            employee1.EndOfYearOperation();
+            employee2.EndOfYearOperation();
+            employee3.EndOfYearOperation();
+            #endregion
         }
     }
 }
---
+++ Assignment 11 - C# - advanced - shahd mostafa/classes/Employee.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_11___C____advanced___shahd_mostafa.classes
+{
+    internal class Employee
+    {
+        public Employee() { }
+        public event EventHandler<EmployeeLayOffEventArgs> EmployeeLayOff;
+        protected virtual void OnEmployeeLayOff (EmployeeLayOffEventArgs e)
+        {
+            EmployeeLayOff?.Invoke(this,e);
+        }
+        public int EmployeeID { get; set; }
+        private DateTime _BirthDate;
+        private int _VacationStock;
+        public DateTime BirthDate
+        {
+            get => _BirthDate;
+            set => _BirthDate = value;
+        }
+        public int VacationStock
+        {
+            get => _VacationStock;
+            set => _VacationStock = value;
+        }
+
+        public bool RequestVacation(DateTime From, DateTime To)
+        {
+            int vacationDays=(To-From).Days;
+            if (VacationStock-vacationDays >0)
+            {
+                VacationStock-=vacationDays;
+                return true;
+            }
+            return false;
+        }
+        public void EndOfYearOperation()
+        {
+            if(VacationStock<0)
+            {
+                OnEmployeeLayOff(new EmployeeLayOffEventArgs(LayOffCause.vacationStockEmpty));
+            }
+            else if(DateTime.Now.Year - BirthDate.Year >60)
+            {
+                OnEmployeeLayOff(new EmployeeLayOffEventArgs(LayOffCause.employeeAgeExceeded));
+            }
+        }
+    }
+    public enum LayOffCause
+    {
+        vacationStockEmpty,
+        employeeAgeExceeded
+    }
+    public class EmployeeLayOffEventArgs
+    {
+        public LayOffCause Cause { get; set; }
+        public EmployeeLayOffEventArgs(LayOffCause cause)
+        {
+            Cause = cause;
+        }
+    }
+
+}
