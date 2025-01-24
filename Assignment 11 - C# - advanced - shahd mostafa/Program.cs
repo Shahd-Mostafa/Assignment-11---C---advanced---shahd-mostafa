cc9c05658e6fc6bbcce42c918d5c02ac8109d974
Shahd  Mostafa <shahd.mustafa124@gmail.com>
2025-01-24 12:45:02 +0200

    Add project files.

---
+++ Assignment 11 - C# - advanced - shahd mostafa/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_11___C____advanced___shahd_mostafa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
